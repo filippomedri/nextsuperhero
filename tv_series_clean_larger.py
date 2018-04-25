ad239ffe6bb182cc0f6c01d5d40bff6a3e4cb5f7
filippomedri <filippo.medri@gmail.com>
2018-04-25 16:53:14 -0700

    Created larger dataset

---
+++ tv_series_clean_larger.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+tv_series_df = pd.read_csv(os.path.join('data','tv_series_final_first_clean'))
+tv_series_df_clean = pd.DataFrame()
+
+at_row = 0
+for index, row in tv_series_df.iterrows():
+    year_list = tv_series_df.loc[index,'Year']
+    title = tv_series_df.loc[index,'Title']
+    year_list = str(year_list).split()
+    for year in year_list:
+        if year != 'nan':
+            int_year = int(year)
+            if 1940 < int_year < 2019:
+                at_row+=1
+                tv_series_df_clean.loc[at_row,'Title'] = title
+                tv_series_df_clean.loc[at_row,'Year']  = int_year
+
+tv_series_df_clean.sort_values('Year',inplace=True)
+tv_series_df_clean.drop_duplicates(inplace=True)
+tv_series_df_clean.reset_index(inplace=True)
+print(tv_series_df_clean.head())
+print(tv_series_df_clean.tail())
+
+tv_series_df_clean.to_csv(os.path.join('data','tv_series_final_clean_larger'))
